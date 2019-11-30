bc65fcd9f4d55ad05fa68bcc0419da91666d2c77
Perotin <maxfuligni@gmail.com>
2019-11-30 16:29:12 -0500

    Finished up base of MainMenu class

--- src/main/java/me/perotin/privatetalk/objects/inventory/paging_objects/MainMenuPaging.java
+++ src/main/java/me/perotin/privatetalk/objects/inventory/paging_objects/MainMenuPaging.java
@@ -5,12 +5,13 @@ import com.github.stefvanschie.inventoryframework.pane.PaginatedPane;
 import me.perotin.privatetalk.PrivateTalk;
 import me.perotin.privatetalk.objects.Chatroom;
 import me.perotin.privatetalk.objects.inventory.PagingMenu;
+import me.perotin.privatetalk.objects.inventory.actions.ChatroomItemStackAction;
 import me.perotin.privatetalk.storage.files.FileType;
 import me.perotin.privatetalk.storage.files.PrivateFile;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,6 +30,7 @@ public class MainMenuPaging extends PagingMenu {
         this.messages = new PrivateFile(FileType.MESSAGES);
         this.pane = new PaginatedPane(2, 1, 7, 4);
         this.plugin = plugin;
+        getPaginatedPane().populateWithGuiItems(generatePages());
 
     }
 
@@ -36,10 +38,24 @@ public class MainMenuPaging extends PagingMenu {
 
     @Override
     protected List<GuiItem> generatePages() {
-        List<GuiItem> items = new ArrayList<>();
         List<ItemStack> toDisplay = plugin.getChatrooms().stream().map(Chatroom::getItem).collect(Collectors.toList());
+        toDisplay = toDisplay.stream().sorted(this::compare).collect(Collectors.toList());
+        List<GuiItem> guiItems = toDisplay.stream().map(item -> new GuiItem(item, ChatroomItemStackAction.clickOnChatroom())).collect(Collectors.toList());
+        return guiItems;
+    }
 
 
-        return items;
+    /**
+     * Sorts the itemstacks by whether or not they have the saved material type, sorting them from saved -> everything else
+     * @param o
+     * @param o1
+     * @return
+     */
+    private int compare(ItemStack o, ItemStack o1){
+        Material saved = Material.valueOf(plugin.getConfig().getString("saved-material"));
+        if(o.getType() == o1.getType()) return 0;
+        else if(o.getType() == saved) return 1;
+        else if(o1.getType() == saved) return -1;
+        return 0;
     }
 }
