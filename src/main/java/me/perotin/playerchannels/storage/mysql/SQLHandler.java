a705f03a289cfd73ddf26e6ff9e1166c424bcea3
perotins <maxfuligni@gmail.com>
2024-08-09 21:22:55 -0400

    add storechatroom mysql port

--- src/main/java/me/perotin/playerchannels/storage/mysql/SQLHandler.java
+++ src/main/java/me/perotin/playerchannels/storage/mysql/SQLHandler.java
@@ -1,5 +1,12 @@
 package me.perotin.playerchannels.storage.mysql;
 
+import me.perotin.playerchannels.objects.Chatroom;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /* Created by Perotin on 8/14/19 */
 public class SQLHandler  {
 
@@ -36,4 +43,46 @@ public class SQLHandler  {
     public int getPort() {
         return port;
     }
+
+
+    public void storeChatroom(Chatroom chatroom) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        try {
+            connection = DriverManager.getConnection(getDatabaseUrl(), username, password);
+
+            String query = "REPLACE INTO chatrooms (ownerUuid, name, description, isPublic, isSaved, nicknamesEnabled, isServerOwned, isGlobal, hidden) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+            statement = connection.prepareStatement(query);
+
+            statement.setString(1, chatroom.getOwner().toString());
+            statement.setString(2, chatroom.getName());
+            statement.setString(3, chatroom.getDescription());
+            statement.setBoolean(4, chatroom.isPublic());
+            statement.setBoolean(5, chatroom.isSaved());
+            statement.setBoolean(6, chatroom.isNicknamesEnabled());
+            statement.setBoolean(7, chatroom.isServerOwned());
+            statement.setBoolean(8, chatroom.isGlobal());
+            statement.setBoolean(9, chatroom.isHidden());
+
+            statement.executeUpdate();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (statement != null) {
+                try { statement.close(); } catch (SQLException e) { /* Ignored */ }
+            }
+            if (connection != null) {
+                try { connection.close(); } catch (SQLException e) { /* Ignored */ }
+            }
+        }
+    }
+
+    private String getDatabaseUrl() {
+        return "jdbc:mysql://" + host + ":" + port + "/" + database + "?useSSL=false&serverTimezone=UTC";
+    }
+
+
 }
